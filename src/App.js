33f7dbf3b53ff820b9840781345bfa820717535d
Şamil NART <samil.nart@gmail.com>
2018-04-25 00:24:21 +0300

    Add area chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PieChart, LineChart, BarChart } from '../../yarcl';
+import { PieChart, LineChart, BarChart, AreaChart } from '../../yarcl';
 import './App.css';
 
 class App extends Component {
@@ -40,6 +40,17 @@ class App extends Component {
                             {name: 'Germany', data: [79, 82, 81]},
                             {name: 'Turkey', data: [54, 63, 78] },
                             {name: 'United Kingdom', data: [57, 58, 65]}
+                        ]}
+                    />
+                </div>
+                <div className="chart">
+                    <AreaChart
+                        title="Population of countries by years (in millions)"
+                        subtitle="in 1950, 1965, 1980, 1995 and 2010"
+                        series={[
+                            {label: 'Turkey', data: [21, 30, 43, 58, 71]},
+                            {label: 'Germany', data: [69, 75, 78, 81, 80]},
+                            {label: 'Canada', data: [14, 19, 24, 29, 34]}
                         ]}
                     />
                 </div>
