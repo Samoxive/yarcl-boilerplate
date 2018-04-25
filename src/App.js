5c9c700416587d9de71138f7f0f90ce6cf765105
Şamil NART <samil.nart@gmail.com>
2018-04-25 20:29:27 +0300

    Use yarcl from npm packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PieChart, LineChart, BarChart, AreaChart, BubbleChart } from '../../yarcl';
+import { PieChart, LineChart, BarChart, AreaChart, BubbleChart } from 'yarcl';
 import './App.css';
 
 class App extends Component {
